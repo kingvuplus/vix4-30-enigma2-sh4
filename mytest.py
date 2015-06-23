4c3822c0d1c4bf1be91e16ba85dd9fd401031b22
mrnamingo <noreply@vix4.com>
2015-06-23 21:46:54 +0100

    Revert "Update mytest.py"
    This reverts commit 3e45668cf8798f1dd0dc960e3283613950a37496.

--- mytest.py
+++ mytest.py
@@ -512,6 +512,8 @@ def runScreenTest():
 
 	profile("RunReactor")
 	profile_final()
+	# kill showiframe if it is running (sh4 hack...)
+	os.system("killall -9 showiframe")
 	runReactor()
 	
 	profile("wakeup")
